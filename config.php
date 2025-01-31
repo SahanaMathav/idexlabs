c8d63b7b9a1523aa0deec018e8d185fece0a2bb6
Sahana69 <Sahanakuhanesh06@gmail.com>
2025-01-31 12:37:12 +0530

    Remove unwanted files

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'awsstart_idexlabs');
-define('DATABASE_PASSWORD', 'bqWa3tbD6yK7ytrQ4hLM');
-define('DATABASE_NAME',     'awsstart_idexlabs');
-define('SITE_URL',          'https://idexlabs.io/');
